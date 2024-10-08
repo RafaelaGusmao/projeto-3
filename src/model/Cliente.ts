0b7fcea810e9186f5fb29e18ede45a902979b33c
Rafaela Gusmão <gusmaorafaela80@gmail.com>
2024-10-08 16:06:02 -0300

    atualizando projeto

--- src/model/Cliente.ts
+++ src/model/Cliente.ts
@@ -1,37 +1,100 @@
+/**
+ * Classe que representa um cliente.
+ */
 export class Cliente {
-    idCliente: number;
-    nome: string;
-    cpf: string;
-    telefone: string;
 
-    constructor(idCliente: number, nome: string, cpf: string, telefone: string) {
-        this.idCliente = idCliente;
+    /* Atributos */
+    /* Identificador do cliente */
+    private idCliente: number = 0;
+    /* Nome do cliente */
+    private nome: string;
+    /* CPF do cliente */
+    private cpf: string;
+    /* Telefone do cliente */
+    private telefone: string;
+
+    /**
+     * Construtor da classe Cliente
+     * 
+     * @param nome Nome do cliente
+     * @param cpf CPF do cliente
+     * @param telefone Telefone do cliente
+     */
+    constructor(nome: string, cpf: string, telefone: string) {
         this.nome = nome;
         this.cpf = cpf;
         this.telefone = telefone;
     }
 
-    // Lista de Clientes
-    listagemClientes(): Array<Cliente> {
-        // Lógica para listar clientes
-        return [];
+    /* Métodos get e set */
+
+    /**
+     * Recupera o identificador do cliente.
+     * @returns o identificador do cliente
+     */
+    public getIdCliente(): number {
+        return this.idCliente;
     }
 
-    // Cadastra um novo cliente
-    cadastroCliente(cliente: Cliente): boolean {
-        // Lógica para cadastrar um novo cliente
-        return true;
+    /**
+     * Atribui um valor ao identificador do cliente.
+     * @param idCliente Novo identificado do cliente
+     */
+    public setIdCliente(idCliente: number): void {
+        this.idCliente = idCliente;
     }
 
-    // Remove um cliente
-    removerCliente(idCliente: number): boolean {
-        // Lógica para remover um cliente
-        return true;
+    /**
+     * Retorna o nome do cliente.
+     * 
+     * @returns {string} O nome do cliente.
+     */
+    public getNome(): string {
+        return this.nome;
     }
 
-    // Atualiza um cliente existente
-    atualizarCliente(cliente: Cliente): boolean {
-        // Lógica para atualizar um cliente
-        return true;
+    /**
+     * Define o nome do cliente.
+     * 
+     * @param nome O nome a ser definido para o cliente.
+     */
+    public setNome(nome: string): void {
+        this.nome = nome;
+    }
+
+    /**
+     * Retorna o CPF do cliente.
+     * 
+     * @returns {string} O CPF do cliente.
+     */
+    public getCpf(): string {
+        return this.cpf;
+    }
+
+    /**
+     * Define o CPF do cliente.
+     * 
+     * @param cpf O CPF a ser definido para o cliente.
+     */
+    public setCpf(cpf: string): void {
+        this.cpf = cpf;
+    }
+
+    /**
+     * Retorna o telefone do cliente.
+     * 
+     * @returns {string} O telefone do cliente.
+     */
+    public getTelefone(): string {
+        return this.telefone;
+    }
+
+    /**
+     * Define o telefone do cliente.
+     * 
+     * @param telefone O telefone a ser definido para o cliente.
+     */
+    public setTelefone(telefone: string): void {
+        this.telefone = telefone;
     }
 }
--- src/model/PedidoVenda.ts
+++ src/model/PedidoVenda.ts
@@ -1,39 +1,122 @@
+/**
+ * Classe que representa um pedido de venda.
+ */
 export class PedidoVenda {
-    idPedido: number;
-    idCarro: number;
-    idCliente: number;
-    dataPedido: Date;
-    valorPedido: number;
 
-    constructor(idPedido: number, idCarro: number, idCliente: number, dataPedido: Date, valorPedido: number) {
-        this.idPedido = idPedido;
+    /* Atributos */
+    /* Identificador do pedido */
+    private idPedido: number = 0;
+    /* Identificador do carro */
+    private idCarro: number;
+    /* Identificador do cliente */
+    private idCliente: number;
+    /* Data do pedido */
+    private dataPedido: Date;
+    /* Valor do pedido */
+    private valorPedido: number;
+
+    /**
+     * Construtor da classe PedidoVenda
+     * 
+     * @param idCarro Identificador do carro
+     * @param idCliente Identificador do cliente
+     * @param dataPedido Data do pedido
+     * @param valorPedido Valor do pedido
+     */
+    constructor(idCarro: number, idCliente: number, dataPedido: Date, valorPedido: number) {
         this.idCarro = idCarro;
         this.idCliente = idCliente;
         this.dataPedido = dataPedido;
         this.valorPedido = valorPedido;
     }
 
-    // Lista de Pedidos
-    listagemPedidos(): Array<PedidoVenda> {
-        // Lógica para listar pedidos
-        return [];
+    /* Métodos get e set */
+
+    /**
+     * Recupera o identificador do pedido.
+     * @returns o identificador do pedido
+     */
+    public getIdPedido(): number {
+        return this.idPedido;
     }
 
-    // Cadastra um novo pedido
-    cadastroPedido(idCliente: number, idCarro: number, dataPedido: Date, valorPedido: number): boolean {
-        // Lógica para cadastrar um novo pedido
-        return true;
+    /**
+     * Atribui um valor ao identificador do pedido.
+     * @param idPedido Novo identificador do pedido
+     */
+    public setIdPedido(idPedido: number): void {
+        this.idPedido = idPedido;
     }
 
-    // Remove um pedido
-    removerPedido(idPedido: number): boolean {
-        // Lógica para remover um pedido
-        return true;
+    /**
+     * Retorna o identificador do carro.
+     * 
+     * @returns {number} O identificador do carro.
+     */
+    public getIdCarro(): number {
+        return this.idCarro;
     }
 
-    // Atualiza um pedido existente
-    atualizarPedido(idPedido: number, idCliente: number, idCarro: number, dataPedido: Date, valorPedido: number): boolean {
-        // Lógica para atualizar um pedido
-        return true;
+    /**
+     * Define o identificador do carro.
+     * 
+     * @param idCarro O identificador do carro a ser definido.
+     */
+    public setIdCarro(idCarro: number): void {
+        this.idCarro = idCarro;
+    }
+
+    /**
+     * Retorna o identificador do cliente.
+     * 
+     * @returns {number} O identificador do cliente.
+     */
+    public getIdCliente(): number {
+        return this.idCliente;
+    }
+
+    /**
+     * Define o identificador do cliente.
+     * 
+     * @param idCliente O identificador do cliente a ser definido.
+     */
+    public setIdCliente(idCliente: number): void {
+        this.idCliente = idCliente;
+    }
+
+    /**
+     * Retorna a data do pedido.
+     * 
+     * @returns {Date} A data do pedido.
+     */
+    public getDataPedido(): Date {
+        return this.dataPedido;
+    }
+
+    /**
+     * Define a data do pedido.
+     * 
+     * @param dataPedido A nova data do pedido.
+     */
+    public setDataPedido(dataPedido: Date): void {
+        this.dataPedido = dataPedido;
+    }
+
+    /**
+     * Retorna o valor do pedido.
+     * 
+     * @returns {number} O valor do pedido.
+     */
+    public getValorPedido(): number {
+        return this.valorPedido;
+    }
+
+    /**
+     * Define o valor do pedido.
+     * 
+     * @param valorPedido O valor do pedido a ser definido.
+     */
+    public setValorPedido(valorPedido: number): void {
+        this.valorPedido = valorPedido;
     }
 }
